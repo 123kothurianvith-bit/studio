d06738fad46aae6f0f92d9822fc7119fdef6eb73
Anvith Kothuri <kothurianvith3@gmail.com>
2025-10-23 12:49:15 +0000

    not that huge lenght make the length some smaller and make them slide

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -117,7 +117,7 @@ function HomePageComponent() {
               >
                   <CarouselContent className="-ml-2">
                       {featuredGames.map((game, index) => (
-                          <CarouselItem key={game.id} className="pl-4 basis-full">
+                          <CarouselItem key={game.id} className="pl-4 basis-full md:basis-1/2">
                               <FeaturedGameCard game={game} index={index} />
                           </CarouselItem>
                       ))}
@@ -153,7 +153,7 @@ function HomePageComponent() {
              <Carousel opts={{align: "start"}} className="w-full">
                 <CarouselContent className="-ml-2">
                     {recommendedGames.map((game, index) => (
-                        <CarouselItem key={`${game.id}-${index}`} className="pl-4 basis-full">
+                        <CarouselItem key={`${game.id}-${index}`} className="pl-4 basis-full md:basis-1/2">
                            <GameCard game={game} variant="default" />
                         </CarouselItem>
                     ))}
@@ -168,7 +168,7 @@ function HomePageComponent() {
             <Carousel opts={{align: "start"}} className="w-full">
                 <CarouselContent className="-ml-2">
                     {popularGames.map((game, index) => (
-                        <CarouselItem key={`${game.id}-${index}`} className="pl-4 basis-full">
+                        <CarouselItem key={`${game.id}-${index}`} className="pl-4 basis-full md:basis-1/2">
                            <GameCard key={game.id} game={game} index={index}/>
                         </CarouselItem>
                     ))}
